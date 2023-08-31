3fe039926cf771aca9dae42a88b23619b2d91194
Ngqabutho K Ndlovu <101482292+kito078@users.noreply.github.com>
2023-08-31 12:21:57 +0200

    x

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import MuiTypography from "./commponents/MuiTypography";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <MuiTypography />
+    </div>
+  );
 }
 
 export default App;
--- src/commponents/MuiTypography.tsx
+++ src/commponents/MuiTypography.tsx
@@ -1,8 +1,17 @@
 import React from "react";
-import { Typography } from "@mui/meterial";
+import Typography from "@mui/material/Typography"; // Import Typography component
 
 function MuiTypography() {
-  return <div>mul</div>;
+  return (
+    <div>
+      <Typography variant="h1">h1 heading</Typography>
+      <Typography variant="h2">h2 heading</Typography>
+      <Typography variant="h3">h3 heading</Typography>
+      <Typography variant="h4">h4 heading</Typography>
+      <Typography variant="h5">h5 heading</Typography>
+      <Typography variant="h6">h6 heading</Typography>
+    </div>
+  );
 }
 
 export default MuiTypography;
