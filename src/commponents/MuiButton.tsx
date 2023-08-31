73957061dc68d1a2b82a7022897769ddda7abf54
Ngqabutho K Ndlovu <101482292+kito078@users.noreply.github.com>
2023-08-31 13:44:43 +0200

    d

--- src/commponents/MuiButton.tsx
+++ src/commponents/MuiButton.tsx
@@ -3,12 +3,50 @@ import { Stack, Button } from "@mui/material";
 
 function MuiButton() {
   return (
-    <Stack spacing={2} direction="row">
-      <Button variant="text" href="https://google.com">
-        Text
-      </Button>
-      <Button variant="contained">contained</Button>
-      <Button variant="outlined">outlined</Button>
+    <Stack spacing={4}>
+      <Stack spacing={2} direction="row">
+        <Button variant="text" href="https://google.com">
+          Text
+        </Button>
+        <Button variant="contained">contained</Button>
+        <Button variant="outlined">outlined</Button>
+      </Stack>
+
+      {/* button colors */}
+
+      <Stack spacing={2} direction="row">
+        <Button variant="contained" color="primary">
+          primary
+        </Button>
+        <Button variant="contained" color="secondary">
+          secondary
+        </Button>
+        <Button variant="contained" color="error">
+          error
+        </Button>
+        <Button variant="contained" color="warning">
+          warnin
+        </Button>
+        <Button variant="contained" color="info">
+          info
+        </Button>
+        <Button variant="contained" color="success">
+          success
+        </Button>
+      </Stack>
+
+      {/* button size */}
+      <Stack display="block" spacing={2} direction="row">
+        <Button variant="contained" size="small">
+          Small
+        </Button>
+        <Button variant="contained" size="medium">
+          medium
+        </Button>
+        <Button variant="contained" size="large">
+          large
+        </Button>
+      </Stack>
     </Stack>
   );
 }
