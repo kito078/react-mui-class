06731efdef87157396535b351c2a62b861fb119c
Ngqabutho K Ndlovu <101482292+kito078@users.noreply.github.com>
2023-08-31 12:54:10 +0200

    f

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import MuiTypography from "./commponents/MuiTypography";
+import MuiBotton from "./commponents/MuiBotton";
 
 function App() {
   return (
     <div className="App">
       <MuiTypography />
+      <MuiBotton />
     </div>
   );
 }
