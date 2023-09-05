1f7900067b7f4ca13965fbd4e5c96dc6c5af11d0
Ngqabutho K Ndlovu <101482292+kito078@users.noreply.github.com>
2023-09-05 11:27:07 +0200

    x

--- src/commponents/MuiRadioButton.js
+++ src/commponents/MuiRadioButton.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   Box,
   FormControl,
@@ -6,23 +6,38 @@ import {
   FormControlLabel,
   RadioGroup,
   Radio,
+  FormHelperText,
 } from "@mui/material";
 
 function MuiRadioButton() {
+  const [value, setValue] = useState("");
+  console.log(value);
+
+  const handleChange = (e) => {
+    setValue(e.target.value);
+  };
   return (
     <Box>
-      <FormControl>
+      <FormControl error>
         <FormLabel id="job-experience-group-label">
           Years of experience
         </FormLabel>
         <RadioGroup
           name="job-experience-group"
           aria-labelledby="job-experience-group-label"
+          value={value}
+          onChange={handleChange}
+          row
         >
-          <FormControlLabel control={<Radio />} label="0-2" value="0-2" />
+          <FormControlLabel
+            control={<Radio size="medium" color="secondary" />}
+            label="0-2"
+            value="0-2"
+          />
           <FormControlLabel control={<Radio />} label="3-5" value="3-5" />
           <FormControlLabel control={<Radio />} label="6-10" value="6-10" />
         </RadioGroup>
+        <FormHelperText>Invalid selection</FormHelperText>
       </FormControl>
     </Box>
   );
