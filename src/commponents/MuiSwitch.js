fcbd1a8a270160d4e0ea2c8982fe3910ed736ebe
Ngqabutho K Ndlovu <101482292+kito078@users.noreply.github.com>
2023-09-12 10:27:44 +0200

    s

--- src/commponents/MuiSwitch.js
+++ src/commponents/MuiSwitch.js
@@ -1,18 +1,25 @@
 import React, { useState } from "react";
-import { Box, FormControlLabel, Switch } from "mui/material";
+import { Box, FormControlLabel, Switch } from "@mui/material";
 
 function MuiSwitch() {
   const [checked, setChecked] = useState(false);
   console.log(checked);
 
   const handleChange = (e) => {
-    setChecked(e.target.value);
+    setChecked(e.target.checked);
   };
   return (
     <Box>
       <FormControlLabel
         label="Dark mode"
-        control={<Switch checked={checked} onChange={handleChange} />}
+        control={
+          <Switch
+            checked={checked}
+            onChange={handleChange}
+            size="small"
+            color="success"
+          />
+        }
       />
     </Box>
   );
