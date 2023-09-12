53a1fae3564463c31f94092e2a5a8c0f837001e0
Ngqabutho K Ndlovu <101482292+kito078@users.noreply.github.com>
2023-09-12 11:12:43 +0200

    e

---
+++ src/commponents/MuiAutocomplete.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import {Stack} from '@mui/material'
+
+function MuiAutocomplete() {
+  return (
+    <Stac spacing={2} width='250px'>
+        Muiauto
+    </Stack>)
+}
+
+export default MuiAutocomplete
--- src/commponents/MuiRating.js
+++ src/commponents/MuiRating.js
@@ -1,17 +1,29 @@
 import React, { useState } from "react";
 import { Stack, Rating } from "@mui/material";
+import { Favorite } from "@mui/icons-material";
+import { FavoriteBorder } from "@mui/icons-material";
 
 function MuiRating() {
-  const [value, setValue] = useState(null);
+  const [value, setValue] = useState(3);
 
   console.log(value);
 
-  const handleChange = () => {
-    setValue(value);
+  const handleChange = (e) => {
+    setValue(e.target.value);
   };
+
   return (
     <Stack>
-      <Rating value={value} onChange={handleChange} />
+      <Rating
+        value={value}
+        onChange={handleChange}
+        precision={0.5}
+        size="large"
+        icon={<Favorite fontSize="inherit" />}
+        emptyIcon={<FavoriteBorder fontSize="inherit" />}
+        // readOnly
+        highlightSelectedOnly
+      />
     </Stack>
   );
 }
