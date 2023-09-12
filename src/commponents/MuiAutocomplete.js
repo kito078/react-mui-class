9ff7243cdb927f330f506a8ec3dde37d5793a3a5
Ngqabutho K Ndlovu <101482292+kito078@users.noreply.github.com>
2023-09-12 15:16:56 +0200

    s

--- src/commponents/MuiAutocomplete.js
+++ src/commponents/MuiAutocomplete.js
@@ -1,17 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import { Stack, Autocomplete, TextField } from "@mui/material";
 
 const skills = ["HTML", "CSS", "JAVASCRIPT", "TYPESCRIPT", "REACT"];
 
 function MuiAutocomplete() {
+  const [value, setValue] = useState(""); // Initialize value as an empty string
+
+  console.log(value);
+
   return (
     <Stack spacing={2} width="250px">
       <Autocomplete
         options={skills}
-        renderInput={(params) => <TextField {...params} label="skills" />}
+        renderInput={(params) => <TextField {...params} label="Skills" />} // Use a proper label
+        value={value}
+        onChange={(e, newValue) => setValue(newValue)}
+        freeSolo
       />
     </Stack>
   );
 }
 
 export default MuiAutocomplete;
+
+// import React, { useState } from "react";
+// import { Stack, Autocomplete, TextField } from "@mui/material";
+
+// const skills = ["HTML", "CSS", "JAVASCRIPT", "TYPESCRIPT", "REACT"];
+
+// function MuiAutocomplete() {
+//   const [value, setValue] = useState(null);
+
+//   console.log(value);
+
+//   return (
+//     <Stack spacing={2} width="250px">
+//       <Autocomplete
+//         options={skills}
+//         renderInput={(params) => <TextField {...params} label="skills" />}
+//         value={value}
+//         onChange={(e) => setValue(e.target.value)}
+//       />
+//     </Stack>
+//   );
+// }
+
+// export default MuiAutocomplete;
