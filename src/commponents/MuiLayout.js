b5e83c90655d69f71019afdf264ae77304d6537e
Ngqabutho K Ndlovu <101482292+kito078@users.noreply.github.com>
2023-09-13 08:36:32 +0200

    s

--- src/commponents/MuiLayout.js
+++ src/commponents/MuiLayout.js
@@ -1,21 +1,64 @@
 import React from "react";
+import { Box, Stack, Divider, Grid, Paper } from "@mui/material";
+
+//BOX Component
+//Team Parlet
 
 function MuiLayout() {
   return (
-    <Box
-      //   component="span"
-      sx={{
-        backgroundColor: "primary.main",
-        color: "white",
-        height: "100px",
-        padding: "16px",
-        "&:hover": {
-          backgroundColor: "primary.light",
-        },
-      }}
-    >
-      revolution
-    </Box>
+    <Paper sx={{padding: '32px'}}>
+      <Stack
+        sx={{ border: "1px solid" }}
+        direction="row"
+        spacing={2}
+        divider={<Divider orientation="vertical" flexItem />}
+      >
+        <Box
+          //   component="span"
+          sx={{
+            backgroundColor: "primary.main",
+            color: "white",
+            width: "100px",
+            height: "100px",
+            padding: "16px",
+            "&:hover": {
+              backgroundColor: "primary.light",
+            },
+          }}
+        >
+          revolution
+        </Box>
+        <Box
+          display="flex"
+          height="100px"
+          width="100px"
+          bgcolor="success.light"
+          p={2}
+        ></Box>
+      </Stack>
+      <Grid container my={4} rowSpacing={2} columnSpacing={1}>
+        <Grid item xs={12} sm={6}>
+          <Box bgcolor="primary.light" p={2}>
+            item 1
+          </Box>
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <Box bgcolor="primary.light" p={2}>
+            item 2
+          </Box>
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <Box bgcolor="primary.light" p={2}>
+            item 3
+          </Box>
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <Box bgcolor="primary.light" p={2}>
+            item 4
+          </Box>
+        </Grid>
+      </Grid>
+    </>
   );
 }
 
