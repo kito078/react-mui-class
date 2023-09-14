ebf5a9a731f19424020a6a967183c8cff4bd3dab
Ngqabutho K Ndlovu <101482292+kito078@users.noreply.github.com>
2023-09-14 13:35:24 +0200

    w

--- src/commponents/MuiCard.js
+++ src/commponents/MuiCard.js
@@ -6,12 +6,19 @@ import {
   Typography,
   CardActions,
   Button,
+  CardMedia,
 } from "@mui/material";
 
 function MuiCard() {
   return (
     <Box width="300px">
       <Card>
+        <CardMedia
+          component="img"
+          height="140"
+          image="https://fishaways.co.za/_next/image?url=%2Fimages%2Fmenu%2Fbetter_for_sharing.png&w=2048&q=75"
+          alt=""
+        />
         <CardContent>
           <Typography gutterBottom variant="h5" component="div">
             React
